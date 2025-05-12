0f053cb7e0059804cd402d96aaa4327ea7fc5df1
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-05-12 11:35:12 +0900

    Feat: 메인 뷰 UI 추가

---
+++ GrowthLog/Features/LogMain/LogMainView.swift
@@ -0,0 +1,19 @@
+//
+//  LogMainView.swift
+//  GrowthLog
+//
+//  Created by Nat Kim on 5/12/25.
+//
+
+import SwiftUI
+
+// MARK: - App 에 뿌려질 메인 뷰
+struct LogMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogMainView()
+}
---
+++ GrowthLog/Features/LogMain/LogMainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LogMainViewModel.swift
+//  GrowthLog
+//
+//  Created by Nat Kim on 5/12/25.
+//
+
+import SwiftUI
+
+// MARK: 메인 화면(작성, 검색, 리스트뷰)
+final class LogMainViewModel: ObservableObject {
+    
+}
