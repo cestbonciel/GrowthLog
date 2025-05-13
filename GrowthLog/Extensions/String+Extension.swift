934a608d67f0ae9857619f10604af56609946860
Seohyun Kim <cestbonciel88@knou.ac.kr>
2025-05-13 09:35:34 +0900

    chore: extension 예시 코드 추가

---
+++ GrowthLog/Extensions/String+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  String+Extension.swift
+//  GrowthLog
+//
+//  Created by Nat Kim on 5/13/25.
+//
+
+import Foundation
+
+/// sample extension 파일입니다.
+/// 확장 추가가 길어질땐 Extensions 폴더에 넣어주시면 됩니다.(선택사항)
+/// 길지 않을 땐 View에 넣어도 상관없습니다.
+extension String {
+    
+}
