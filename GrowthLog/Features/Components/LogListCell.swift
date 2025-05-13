dff80bcdd4f851cc0bbdae949a5408d3d433d1e9
twoweeks-y <twoweeks1008@gmail.com>
2025-05-13 20:35:54 +0900

    feat: 메인 회고 리스트 / 상세 화면 구현

---
+++ GrowthLog/Features/Components/LogListCell.swift
@@ -0,0 +1,18 @@
+//
+//  LogListCell.swift
+//  GrowthLog
+//
+//  Created by JuYong Lee on 5/13/25.
+//
+
+import SwiftUI
+
+struct LogListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogListCell()
+}
